69b46fc0f304ee2382ce6dfff3ceea09e7013064
Ulku <ulkuislamoglu@gmail.com>
2019-11-20 11:29:07 -0600

    ss

--- src/test.java
+++ src/test.java
@@ -1,6 +1,13 @@
+import javax.management.StringValueExp;
+import java.util.Arrays;
+
 public class test {
     public static void main(String[] args) {
         String name="bugun hava cok guzel";
+        String []arr=name.split(" ");
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(name);
         StringBuilder sb=new StringBuilder(name);
         sb.reverse();
         System.out.println(sb);
