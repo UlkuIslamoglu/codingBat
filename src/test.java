f4cacf21b11b994c4ce637c8cfa546afc6c00ba1
Ulku <ulkuislamoglu@gmail.com>
2019-11-20 11:23:57 -0600

    ss

--- src/test.java
+++ src/test.java
@@ -1,2 +1,8 @@
 public class test {
+    public static void main(String[] args) {
+        String name="bugun hava cok guzel";
+        StringBuilder sb=new StringBuilder(name);
+        sb.reverse();
+        System.out.println(sb);
+    }
 }
